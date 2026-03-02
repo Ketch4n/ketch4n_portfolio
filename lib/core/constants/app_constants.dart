078d64160eebb55b340c7953d5b29a27e6ed4198
Ketch4n <mangao.christian.04@gmail.com>
2026-03-03 00:06:55 +0800

    fix: Assets failed to load on pages

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   AppConstants._(); // Prevent instantiation
 
   static const String appName = "Ketch4n";
-  static const String bgUrl = "dev/night.jpg";
-  static const String flutterLogo = "icon_logo/flutter.png";
-  static const String profileImg = "dev/noob.png";
+  static const String bgUrl = "assets/dev/night.jpg";
+  static const String flutterLogo = "assets/icon_logo/flutter.png";
+  static const String profileImg = "assets/dev/noob.png";
 }
--- lib/core/constants/hexagon_icons_group_constants.dart
+++ lib/core/constants/hexagon_icons_group_constants.dart
@@ -11,49 +11,49 @@ class HexagonIconsGroupContants {
   static const String uiuxConst = "UI / UX";
 
   static const mainStack = <SkillIconEntity>[
-    SkillIconEntity(title: 'Flutter', icon: 'logo/flutter.png'),
-    SkillIconEntity(title: 'Laravel', icon: 'logo/laravel.png'),
-    SkillIconEntity(title: 'Angular', icon: 'logo/angular.png'),
+    SkillIconEntity(title: 'Flutter', icon: 'assets/logo/flutter.png'),
+    SkillIconEntity(title: 'Laravel', icon: 'assets/logo/laravel.png'),
+    SkillIconEntity(title: 'Angular', icon: 'assets/logo/angular.png'),
   ];
 
   static const backend = <SkillIconEntity>[
-    SkillIconEntity(title: 'Firebase', icon: 'logo/firebase.png'),
-    SkillIconEntity(title: 'MySQL', icon: 'logo/mySQL.png'),
-    SkillIconEntity(title: 'Csharp', icon: 'logo/csharp.png'),
+    SkillIconEntity(title: 'Firebase', icon: 'assets/logo/firebase.png'),
+    SkillIconEntity(title: 'MySQL', icon: 'assets/logo/MySQL.png'),
+    SkillIconEntity(title: 'Csharp', icon: 'assets/logo/csharp.png'),
   ];
 
   static const cicd = <SkillIconEntity>[
-    SkillIconEntity(title: 'Git', icon: 'logo/git.png'),
-    SkillIconEntity(title: 'CodeMagic', icon: 'logo/codemagic.png'),
-    SkillIconEntity(title: 'Azure', icon: 'logo/azure.png'),
+    SkillIconEntity(title: 'Git', icon: 'assets/logo/git.png'),
+    SkillIconEntity(title: 'CodeMagic', icon: 'assets/logo/codemagic.png'),
+    SkillIconEntity(title: 'Azure', icon: 'assets/logo/azure.png'),
   ];
 
   static const tools = <SkillIconEntity>[
-    SkillIconEntity(title: 'VS Code', icon: 'logo/vscode.png'),
-    SkillIconEntity(title: 'Docker', icon: 'logo/docker.png'),
-    SkillIconEntity(title: 'Postman', icon: 'logo/postman.png'),
+    SkillIconEntity(title: 'VS Code', icon: 'assets/logo/vscode.png'),
+    SkillIconEntity(title: 'Docker', icon: 'assets/logo/docker.png'),
+    SkillIconEntity(title: 'Postman', icon: 'assets/logo/postman.png'),
   ];
 
   static const stateManagement = <SkillIconEntity>[
-    SkillIconEntity(title: 'Bloc', icon: 'logo/bloc.png'),
-    SkillIconEntity(title: 'Riverpod', icon: 'logo/riverpod.png'),
-    SkillIconEntity(title: 'Provider', icon: 'logo/dart.png'),
+    SkillIconEntity(title: 'Bloc', icon: 'assets/logo/bloc.png'),
+    SkillIconEntity(title: 'Riverpod', icon: 'assets/logo/riverpod.png'),
+    SkillIconEntity(title: 'Provider', icon: 'assets/logo/dart.png'),
   ];
 
   static const uiux = <SkillIconEntity>[
     // SkillIconEntity(title: 'Ollama', icon: 'logo/ollama2.png'),
-    SkillIconEntity(title: 'Photoshop', icon: 'logo/adobe-ps.png'),
-    SkillIconEntity(title: 'Figma', icon: 'logo/figma.png'),
+    SkillIconEntity(title: 'Photoshop', icon: 'assets/logo/adobe-ps.png'),
+    SkillIconEntity(title: 'Figma', icon: 'assets/logo/figma.png'),
   ];
 
   static const ml = <SkillIconEntity>[
-    SkillIconEntity(title: 'TensorFlow', icon: 'logo/tensorflow.png'),
-    SkillIconEntity(title: 'Ollama', icon: 'logo/ollama2.png'),
-    SkillIconEntity(title: 'HuggingFace', icon: 'logo/hf-logo.png'),
+    SkillIconEntity(title: 'TensorFlow', icon: 'assets/logo/tensorflow.png'),
+    SkillIconEntity(title: 'Ollama', icon: 'assets/logo/ollama2.png'),
+    SkillIconEntity(title: 'HuggingFace', icon: 'assets/logo/hf-logo.png'),
   ];
 
   static const ide = <SkillIconEntity>[
-    SkillIconEntity(title: 'VS', icon: 'logo/visual-studio.png'),
-    SkillIconEntity(title: 'Android', icon: 'logo/android-studio.png'),
+    SkillIconEntity(title: 'VS', icon: 'assets/logo/visual-studio.png'),
+    SkillIconEntity(title: 'Android', icon: 'assets/logo/android-studio.png'),
   ];
 }

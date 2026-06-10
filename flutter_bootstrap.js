c1abf509e068f35ffce757a629b316c7160378e2
Ketch4n <129828848+Ketch4n@users.noreply.github.com>
2026-06-10 16:34:11 +0000

    Deploying to gh-pages from @ Ketch4n/ketch4n_portfolio@e2363d94ee8320687d0afcd2a4f083cbc86f987d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "802609722" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1496148076" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
